05b901689add5a6bac75108ac12f4812855ffa06
Digilab-jdeblander <58036816+Digilab-jdeblander@users.noreply.github.com>
2019-12-13 15:03:53 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ class Showreports(MycroftSkill):
     def handle_showreports(self, message):
         self.speak_dialog('Opening the report')
         dir_file = "/home/sopra/Downloads/OpenPowerBi.html"
-        webbrowser.open('file://' + dir_file)
+        webbrowser.Chromium('file://' + dir_file)
 
 
 def create_skill():
