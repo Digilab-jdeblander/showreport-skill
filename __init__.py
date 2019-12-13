151c9eef03eba17d45d07015763bf4f7945bb10c
Digilab-jdeblander <58036816+Digilab-jdeblander@users.noreply.github.com>
2019-12-13 12:05:20 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,17 @@
 from mycroft import MycroftSkill, intent_file_handler
 
 
-class Showreport(MycroftSkill):
+class Showreports(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('showreport.intent')
-    def handle_showreport(self, message):
-        self.speak_dialog('showreport')
+ 
+
+    @intent_file_handler('showreports.intent')
+    def handle_showreports(self, message):
+        self.speak_dialog('Opening the report')
+        dir_file = "/home/sopra/Downloads/OpenPowerBi.html"
+        webbrowser.open('file://' + dir_file)
 
 
 def create_skill():
